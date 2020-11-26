ac0fc6de6a6ca7b1cb6624f53b1dbaa06ed29274
kemal asci <66476984+kemalasci@users.noreply.github.com>
2020-11-26 12:20:47 +0100

    Yeni Sepete ekleme islemi yapildi

--- src/test1.java
+++ src/test1.java
@@ -1,6 +1,7 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("urunlere listele islemi yapildi");
-        System.out.println("Sepete ekleme islemi yapildi");
+        System.out.println("Yeni Sepete ekleme islemi yapildi");
+        System.out.println("Yeni Sepete ekleme 2 islemi yapildi");
     }
 }
